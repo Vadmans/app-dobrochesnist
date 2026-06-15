366e788347bc55dc318ee5a7fb77a4963494a036
Vadmans <91942918+Vadmans@users.noreply.github.com>
2026-06-15 12:14:24 +0300

    Delete apk+logo/vite.config.js

--- apk+logo/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// base: "./" — обов'язково для Capacitor, щоб шляхи до файлів були відносні
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  build: { outDir: "dist" },
-});
